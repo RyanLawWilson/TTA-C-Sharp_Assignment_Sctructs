2184c8e2cb5a9573f422eae085376957199f484c
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-03-03 15:43:43 -0700

    Add project files.

---
+++ Sctructs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sctructs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, there...");
+            System.Threading.Thread.Sleep(1500);
+
+            Console.WriteLine("I'm going to create my Struct");
+            Number num = new Number();
+            Random rand = new Random();
+            num.Amount = (decimal)rand.NextDouble() * 100000;
+
+            System.Threading.Thread.Sleep(1500);
+            Console.WriteLine("Here it is: " + num.Amount);
+
+            Console.Read();
+        }
+    }
+}
